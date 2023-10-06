c24aa24ab75520267cc47ce3dd469cf313bb890b
Ishan Nikeshala <ishannikeshala99@gmail.com>
2023-10-06 11:02:15 +0530

    design update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { headerLogo } from '../assets/images'
-// import { hamburger } from '../assets/icons'
 import { navLinks } from '../constants'
+import { hamburger } from '../assets/icons'
 
 const Nav = () => {
     return (
@@ -10,13 +10,16 @@ const Nav = () => {
                 <a href='/'>
                     <img src={headerLogo} alt='Logo' width={130} height={29} />
                 </a>
-                <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden '>
                     {navLinks.map((item) => (
-                        <li key={item.label}>
-                            <a href={item.href} className='font-montserrat leading-normal text-slate-gray'>{item.label}</a>
+                        <li key={item.label} >
+                            <a href={item.href} className='font-montserrat leading-normal text-slate-gray font-medium'>{item.label}</a>
                         </li>
                     ))}
                 </ul>
+                <div className='hidden max-lg:block'>
+                    <img src={hamburger} width={25} height={25} alt='Humburger'/>
+                </div>
             </nav>
         </header>
     )
