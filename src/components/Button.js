e233e98768de32552b9fd0d99b865d759474b8a4
Ishan Nikeshala <ishannikeshala99@gmail.com>
2023-10-07 14:04:15 +0530

    hero page design and design update

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { arrowRight } from '../assets/icons';
+// import { arrowRight } from '../assets/icons';
 
-const Button = () => {
+const Button = ({label,iconURL}) => {
     return (
-        <Button>Shop now
-            <img src={arrowRight} alt='arrow roght icon' className='ml-2 rounded-full w-5 h-5' />
-        </Button>
+        <button className="flex justify-center items-center gap-2 px-7 leading-none bg-coral-red rounded-full text-white py-3">{label}
+        <img src={iconURL} alt="arrow right icon" className="w-5 h-5 rounded-full"/>
+        </button>
     )
 }
 
